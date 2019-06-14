6b66dbf876a5f1787327880ff1b5957a3fcee4f4
Pratik Savla <pratiksavla07@gmail.com>
2019-06-14 18:28:37 +0530

    Update data_loader.py

--- util/data_loader.py
+++ util/data_loader.py
@@ -77,7 +77,8 @@ class DataLoader():
                     volume_name = img_file_dir + '/' + img_file_dir + '.' + self.file_postfix
                     break
         assert(volume_name is not None)
-        volume_name = os.path.join(patient_dir, volume_name)
+        #volume_name = os.path.join(patient_dir, volume_name)
+        volume_name = data/BRATS2015_Training/HGG/brats_tcia_pat105_0001/VSD.Brain.XX.O.MR_T1.35536.mha
         volume = load_3d_volume_as_array(volume_name)
         return volume, volume_name
 
